d0f875db22433bdec1a621917db22ff84c1814ff
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-05-25 16:41:55 +0430

    Level Text UI added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private Rigidbody Rigidbody;
     private PlayerController PlayerController;
     private ColorManager ColorManager;
+    private int FloorCubesNum;
+
+    void Awake()
+    {
+        Text Level = FindObjectOfType<Text>();
+        Level.text = SceneManager.GetActiveScene().name;
+    }
 
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
         PlayerController = GetComponent<PlayerController>();
         ColorManager = GetComponent<ColorManager>();
+        FloorCubesNum = GameObject.FindGameObjectsWithTag("Floor").Length;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -27,9 +37,18 @@ public class GameManager : MonoBehaviour
         }
         else if (collision.collider.tag == "Floor")
         {
-            print("Floor");
             ColorManager.ChangeFloorColor(collision.collider.gameObject);
             collision.collider.tag = "ColoredBefore";
+            FloorCubesNum --;
+            if (FloorCubesNum == 0)
+            {
+                GotoNextLevel();
+            }
         }
+    }
+
+    private void GotoNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
